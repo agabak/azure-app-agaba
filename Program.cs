a97b087f550807ee803a8c34190ce6b833e07721
agabak <agaba_k@hotmail.com>
2025-02-12 13:21:19 -0600

    add staging to the server

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ app.MapGet("/", () => "Hello, from the web");
 app.Run(async (HttpContext httpContext) =>
 {
     httpContext.Response.StatusCode = 400;
-    await httpContext.Response.WriteAsync("Hello from the other  work,  will deploy the changes- both stating and dev");
+    await httpContext.Response.WriteAsync("Hello from the other  work,  will deploy the changes- both stating");
 });
 
 app.Run();
