53377b3b4186c6d54f3f7c2f251a3a71aaaf3564
agabak <agaba_k@hotmail.com>
2025-02-12 11:51:50 -0600

    new changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ app.MapGet("/", () => "Hello, from the web");
 app.Run(async (HttpContext httpContext) =>
 {
     httpContext.Response.StatusCode = 400;
-    await httpContext.Response.WriteAsync("Hello from the other  work,  will deploy the changes");
+    await httpContext.Response.WriteAsync("Hello from the other  work,  will deploy the changes- both stating and dev");
 });
 
 app.Run();
