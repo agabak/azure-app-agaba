c9b9f5ea41e63afeef8009f74414453b897d25f8
agabak <agaba_k@hotmail.com>
2025-02-12 11:09:50 -0600

    check in the changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ app.MapGet("/", () => "Hello, from the web");
 app.Run(async (HttpContext httpContext) =>
 {
     httpContext.Response.StatusCode = 400;
-    await httpContext.Response.WriteAsync("Hello there from http word");
+    await httpContext.Response.WriteAsync("Hello from the other  work");
 });
 
 app.Run();
